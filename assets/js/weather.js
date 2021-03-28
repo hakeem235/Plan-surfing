e06a1b9ebe11f8e869442269f5bcb5275cd14b0f
ahmed hakeem <hakeemkoo@hotmail.com>
2021-03-28 01:33:43 -0400

    updeta the fils

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,3 +1,18 @@
+$('#weatherBtn').on('click', function () {
+    $('#dailyTasks').hide();
+    $('.sectionImg').hide();
+    $('.quote-wrapper').hide();
+    $('.between').hide();
+    $('#news').hide();
+    $('#w3Cell').hide();
+    $('#humidity').show();
+    $('#wind-speed').show()
+
+  })
+
+
+
+
 var apiKey = "cd7b46258d74ce0db68ce9bc4d5cbf2e";
 var rquestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Ottawa&units=metric&appid=' + apiKey;
 
@@ -14,8 +29,8 @@ function mobileWesther(rquestUrl) {
             $('#cityName').text(data.name);
             $("<img>").attr("src", iconUrl).appendTo("#cityName")
             $('#temperature').text("Temperature: " + data.main.temp.toFixed(0) + ' °C');
-            $('#humidity').text("Humidity: " + data.main.humidity + "%");
-            $('#wind-speed').text("Wind Speed: " + data.wind.speed.ap + ' MPH');
+            $('#humidity').text("Humidity: " + data.main.humidity + "%").hide();
+            $('#wind-speed').text("Wind Speed: " + data.wind.speed + ' MPH').hide();
         })
 }
 // call the funtions section 
