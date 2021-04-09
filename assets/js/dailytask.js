639e2f88ec9586590bc9ffc6a867b3297bb3b6a1
Anthony Ndegwa <anthon.n24@gmail.com>
2021-04-09 14:16:42 -0400

    update

--- assets/js/dailytask.js
+++ assets/js/dailytask.js
@@ -44,14 +44,18 @@ function openModal(date) {
 
 function load() {
   const dt = new Date();
+
   if (nav !== 0) {
     dt.setMonth(new Date().getMonth() + nav);
   }
+
   const day = dt.getDate();
   const month = dt.getMonth();
   const year = dt.getFullYear();
+
   const firstDayOfMonth = new Date(year, month, 1);
   const daysInMonth = new Date(year, month + 1, 0).getDate();
+  
   const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
     weekday: 'long',
     year: 'numeric',
@@ -60,11 +64,12 @@ function load() {
   });
   const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
 
+
   document.getElementById('monthDisplay').innerText = 
     `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
 
   calendar.innerHTML = '';
-  
+
   for (let i = 1; i <= paddingDays + daysInMonth; i++) {
     const daySquare = document.createElement('div');
     daySquare.classList.add('day');
@@ -121,19 +126,28 @@ function deleteEvent() {
   localStorage.setItem('events', JSON.stringify(events));
   closeModal();
 }
+function deleteEvent() {
+  events = events.filter(e => e.date !== clicked);
+  localStorage.setItem('events', JSON.stringify(events));
+  closeModal();
+}
+
 function initButtons() {
   document.getElementById('nextButton').addEventListener('click', () => {
     nav++;
     load();
   });
+
   document.getElementById('backButton').addEventListener('click', () => {
     nav--;
     load();
   });
+
   document.getElementById('saveButton').addEventListener('click', saveEvent);
   document.getElementById('cancelButton').addEventListener('click', closeModal);
   document.getElementById('deleteButton').addEventListener('click', deleteEvent);
   document.getElementById('closeButton').addEventListener('click', closeModal);
 }
+
 initButtons();
 load();
